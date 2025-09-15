86e8326a3bc65d844408bf8b895db0ee7c2d990b
이찬희 <leechanhye3722@gmail.com>
2025-09-15 19:27:18 +0900

    update

--- models/ResNet.py
+++ models/ResNet.py
@@ -3,19 +3,19 @@ import torch
 
 class ResidualBlock(nn.Module):
     def __init__(self,
-                 in_dim: int,
-                 out_dim: int,
+                 in_channels: int,
+                 out_channels: int,
                  stride: int = 1,
                  kernel_size: int = 3
                  ):
         super().__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(in_dim, out_dim, kernel_size = 3, padding = 0, stride = stride, bias = False),
-            nn.BatchNorm2d(out_dim),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=0, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_dim, out_dim, kernel_size = 3, padding = 1, stride = 1, bias = False),
-            nn.BatchNorm2d(out_dim)
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels)
         )
 
         self.relu = nn.ReLU()
@@ -31,22 +31,22 @@ class ResidualBlock(nn.Module):
 
 class Bottleneck(nn.Module):
     def __init__(self,
-                 in_dim: int,
-                 out_dim: int,
+                 in_channels: int,
+                 out_channels: int,
                  stride: int = 1,
                  kernel_size: int = 3
                  ):
         super().__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(in_dim, out_dim, kernel_size = 1, padding = 0, stride = stride, bias = False),
-            nn.BatchNorm2d(out_dim),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_dim, out_dim, kernel_size = 3, padding =1, stride = 1, bias = False),
-            nn.BatchNorm2d(out_dim),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_dim, 4 * out_dim, kernel_size = 1, padding =1, stride = 1, bias = False),
-            nn.BatchNorm2d(4 * out_dim)
+            nn.Conv2d(out_channels, 4 * out_channels, kernel_size=1, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(4 * out_channels)
         )
 
         self.relu = nn.ReLU()
@@ -62,5 +62,15 @@ class Bottleneck(nn.Module):
     
 
 class ResNetBlock(nn.Module):
-    pass
+    def __init__(self, block:nn.Module,
+                 in_channels: int,
+                 out_channels: int,
+                 num_layers: int,
+                 stride: int,
+                 downsample: nn.Module = None
+                 ):
+        pass
+        
+
+
     
